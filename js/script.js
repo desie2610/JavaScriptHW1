e95178dd552d915f92a562db69da314e82dfaaed
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-03-18 19:48:12 +0200

    HW1

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const age = 13;
+console.log (age)
+
+// ====================
+
+const name = "Tymofii";
+console.log (name);
+
+// ====================
+
+const  isStudent = true;
+console.log ( isStudent);
+
+// ====================
+
+const myString = "Дають бери не дають відбери :-)";
+console.log (myString);
+
+// ====================
+
+const myNumber = 1;
+const plus = myNumber + 10;
+console.log (plus);
+
+// ====================
+
+const myNull = null;
+console.log (myNull);
+
+// ====================
+
+const userName = prompt("Введіть своє ім’я:");
+alert ("Ваше ім’я:" + userName);
+console.log (typeof (userName) );
+
+// ====================
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?")
+console.log (userAnswer);
+
+
+// ====================
+
+alert("Увага! Дія може бути небезпечною.");
+const confirmation = confirm("Ви впевнені, що хочете продовжити?");
+console.log (confirmation);
+
+
+
+ 
+ 
+
+
